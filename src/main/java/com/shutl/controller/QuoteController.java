26d9bc6d11064b36b81c6c0e4819b4a357222c43
Dimiftb <bukichoni@gmail.com>
2019-05-04 06:14:00 +0100

    Final

--- src/main/java/com/shutl/controller/QuoteController.java
+++ src/main/java/com/shutl/controller/QuoteController.java
@@ -1,20 +1,34 @@
 package com.shutl.controller;
 
 import com.shutl.model.Quote;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@RestController
+@Controller
 public class QuoteController {
 
     @RequestMapping(value = "/quote", method = POST)
-    public @ResponseBody Quote quote(@RequestBody Quote quote) {
-        Long price = Math.abs((Long.valueOf(quote.getDeliveryPostcode(), 36) - Long.valueOf(quote.getPickupPostcode(), 36))/100000000);
+    public
+    String quote( Quote quote, Model model) {
+        model.addAttribute("quote" , new Quote(quote.getPickupPostcode(), quote.getDeliveryPostcode(), quote.getVehicle(), quote.calc_delivery_cost()));
+        return "response";
+    }
 
-        return new Quote(quote.getPickupPostcode(), quote.getDeliveryPostcode(), price);
+    @RequestMapping(value = "/quote", method = GET)
+    public String redirect(Model model) {
+        model.addAttribute("index", new Quote());
+        return "index";
+    }
+
+    @RequestMapping(value = "/", method = GET)
+    public String welcome(Model model) {
+        model.addAttribute("index", new Quote());
+        return "index";
     }
 }
--- src/main/java/com/shutl/model/Quote.java
+++ src/main/java/com/shutl/model/Quote.java
@@ -1,21 +1,62 @@
 package com.shutl.model;
 
 public class Quote {
-    String pickupPostcode;
-    String deliveryPostcode;
-    Long price;
 
-    public Quote() {}
+    private String pickupPostcode;
+    private String deliveryPostcode;
+    private Long price;
+    private String vehicle;
 
-    public Quote(String pickupPostcode, String deliveryPostcode) {
-        this.pickupPostcode = pickupPostcode;
-        this.deliveryPostcode = deliveryPostcode;
+    public Quote() {
     }
 
-    public Quote(String pickupPostcode, String deliveryPostcode, Long price) {
+
+    public Quote(String pickupPostcode, String deliveryPostcode, String vehicle) {
+        this.pickupPostcode = pickupPostcode;
+        this.deliveryPostcode = deliveryPostcode;
+        this.vehicle = vehicle;
+    }
+
+
+    public Quote(String pickupPostcode, String deliveryPostcode, String vehicle, Long price) {
         this.pickupPostcode = pickupPostcode;
         this.deliveryPostcode = deliveryPostcode;
         this.price = price;
+        this.vehicle = vehicle;
+    }
+
+    public Long calc_delivery_cost() {
+        Double markup;
+        switch (vehicle) {
+            case "bicycle":
+                markup = 1.1;
+                break;
+            case "motorbike":
+                markup = 1.15;
+                break;
+            case "parcel_car":
+                markup = 1.20;
+                break;
+            case "small_van":
+                markup = 1.30;
+                break;
+            case "large_van":
+                markup = 1.40;
+                break;
+            default:
+                markup = 1.0;
+        }
+
+        String s1 = deliveryPostcode.replaceAll("\\s", "");
+        String s2 = pickupPostcode.replaceAll("\\s", "");
+        price = Math.abs((Long.valueOf(s1, 36) - Long.valueOf(s2, 36)) / 100000000);
+
+        /*
+         *  delivery fee between identical postcodes would always be 0
+         *  and since we shouldn't use geolocation I've simply added a minimum fee of 1
+         */
+
+        return price < 1 ? (Math.round(1 * markup)) : (Math.round(price * markup));
     }
 
     public String getPickupPostcode() {
@@ -40,5 +81,13 @@ public class Quote {
 
     public void setPrice(Long price) {
         this.price = price;
+    }
+
+    public String getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(String vehicle) {
+        this.vehicle = vehicle;
     }
 }
